84c720042c94fb296dd496da5eb5b233af50c930
shawlyalice <shawly.aust@gmail.com>
2023-01-16 20:19:57 +0600

    fd

---
+++ GYMONE/GYMONE/Controllers/AboutusController.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using GYMONE.Models;
+using Dapper;
+using WebMatrix.WebData;
+using GYMONE.Filters;
+using GYMONE.Repository;
+
+namespace GYMONE.Controllers
+{
+    public class AboutusController : Controller
+    {
+        //
+        // GET: /Aboutus/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
